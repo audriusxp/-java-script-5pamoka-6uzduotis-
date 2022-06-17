b023cbdac7cd11b007ec7c4f4e54a940706fe1a6
audriusxp <palm@inbox.lt>
2022-06-17 15:22:49 +0300

    patikrinta ar masyve yra skaicius 12-gautas rezulatas True

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 window.onload =  function(){
-const masyvas =[5,12,3,6,8,14,15,5,6,10]
+const masyvas =[5,12,3,6,8,14,15,5,6,10];
+console.log(masyvas);
+console.log(ieskomasSkaicius(masyvas));
 }
+
+function ieskomasSkaicius(masyvas){
+
+    for (let i = 0; i < masyvas.length; i++) 
+    {
+       if (lygu(masyvas[i]))
+     {
+      return true;
+    }
+      else 
+     {
+      return false;
+     } 
+    }
+
+        
+   }
+function lygu(x)
+{
+    return x= 12; 
+} 
